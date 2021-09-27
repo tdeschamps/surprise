bcdca68acaa1019829e8acbfb5f31c70b86f1c60
Thomas Deschamps <thomasdscmps@gmail.com>
2021-09-27 10:36:52 +0200

    awww damn

--- surprise.rb
+++ surprise.rb
@@ -1,3 +1,7 @@
 class Surprise
   PASSWORD = '7d83csb9-k4l8-32s2-jd68-i5hcs6f45bfy'.freeze
+  database = 'toto_test'
+  username = '1qo09orzy43m'
+  host = 'ud2osl44p9n9.us-east-2.psdb.cloud'
+  password = 'pscale_pw_TJRlyQWJietrCX3fUBIdOXxiZUbfITd8QyX9iwjGPAw'
 end

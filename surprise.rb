aa0a5e7980c62898fca623d17b19c4bafbf6e753
Thomas Deschamps <thomasdscmps@gmail.com>
2021-11-24 09:37:12 +0100

    you won't scale

--- surprise.rb
+++ surprise.rb
@@ -3,5 +3,5 @@ class Surprise
   database = 'toto_test'
   username = '1qo09orzy43m'
   host = 'ud2osl44p9n9.us-east-2.psdb.cloud'
-  password = 'pscale_pw_TJRlyQWJietrCX3fUBIdOXxiZUbfITd8QyX9iwjGPAw'
+  password = 'pscale_pw_TJRlyQWJietrCX3fUBIdOXxiZUbfITd8QyX9ieckljwf'
 end

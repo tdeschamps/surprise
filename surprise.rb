bceaadcc43568439393a07cfdb44c5f42bd59fd7
Thomas Deschamps <thomasdscmps@gmail.com>
2022-01-24 15:00:16 +0100

    :pizza:

--- surprise.rb
+++ surprise.rb
@@ -3,5 +3,5 @@ class Surprise
   database = 'toto_test'
   username = '1qo09orzy43m'
   host = 'ud2osl44p9n9.us-east-2.psdb.cloud'
-  password = 'pscale_pw_TJRlyQWJietrCX3fFCjs2XxiZUbfITd8QyX9iecscjw'
+  password = 'pscale_pw_TJRlyJAJietrCX3fFCjs2XxiZUbfITd8QyX9iecscjw'
 end
